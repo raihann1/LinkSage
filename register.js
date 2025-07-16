8027717e7186a5585923e60ced5173bf33716eb6
raihann1 <72947239+raihann1@users.noreply.github.com>
2025-07-16 18:51:41 -0400

    add register file to load slash commands into guild for testing

---
+++ register.js
@@ -0,0 +1,27 @@
+const { Client, GatewayIntentBits } = require("discord.js");
+require("dotenv").config();
+const {
+  SlashCommandBuilder,
+} = require("@discordjs/builders");
+const client = new Client({
+  intents: [GatewayIntentBits.GuildMembers, GatewayIntentBits.Guilds],
+});
+
+client.on("ready", async () => {
+  console.log("Successfully connected! Running registration to load slash commands now...")
+  const guildID = process.env.TEST_SERVER
+  const guild = await client.guilds.fetch(guildID);
+  const pingCmd = new SlashCommandBuilder()
+    .setName("ping")
+    .setDescription("Replies with Pong!");
+  await guild.commands.set([
+    pingCmd.toJSON(),
+    appealCmd.toJSON(),
+    debugCmd.toJSON(),
+  ]);
+  // Guild-only commands while in development
+  await client.application?.commands.set([]);
+  console.log("Slash commands loaded into guild!");
+});
+
+client.login(process.env.DISCORD_BOT_TOKEN);
