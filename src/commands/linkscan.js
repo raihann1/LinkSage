2bc91f38aa574c20a5a19cb13df0cdbd7176d106
raihann1 <72947239+raihann1@users.noreply.github.com>
2025-07-22 18:35:15 -0400

    add support for links w/ out subdomains

--- src/commands/linkscan.js
+++ src/commands/linkscan.js
@@ -1,10 +1,11 @@
 import { replyInteraction } from "../methods"
 export async function linkScan(interaction, link) {
     // check if the link is valid
-   if (!link || !/^(https?:\/\/[^\s]+|www\.[^\s]+)$/i.test(link)) {
-        await replyInteraction(interaction, "Invalid link provided. Please provide a valid URL.", true);
-        return;
-    } else {
-        // Link scanning logic, check using external API and native methods
-    }
+if (!link || !/^((https?:\/\/)?(www\.)?)?([a-zA-Z0-9-]+\.)*[a-zA-Z0-9-]+\.[a-zA-Z]{2,}(\/[^\s]*)?$/i.test(link)) {
+    await replyInteraction(interaction, "Invalid link provided. Please provide a valid URL.", true);
+    return;
+} else {
+    // Link scanning logic, check using external API and native methods
+    await replyInteraction(interaction, `W.I.P`, true);
+}
 }
