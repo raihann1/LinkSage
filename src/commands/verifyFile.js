fc7beccdb1589f7abf40ac5dd4db5797dc390284
raihann1 <72947239+raihann1@users.noreply.github.com>
2025-08-16 18:41:17 -0400

    setup verifyFile logic

--- src/commands/verifyFile.js
+++ src/commands/verifyFile.js
@@ -1,4 +1,17 @@
+import { replyInteraction } from "../methods";
 export async function verifyFile(interaction) {
+    // fetch attachment from interaction data
+    const attachment = interaction.data?.resolved?.messages?.[interaction.data.target_id]?.attachments;
+    if (!attachment) {
+        return await replyInteraction(interaction, "No file attached to the message.", true);
+    }
+    // fetch attachments, even if multiple
+    const files = attachment.map(file => file.url);
+    if (files.length === 0) {
+        return await replyInteraction(interaction, "No files found in the message.", true);
+    } else {
+         await replyInteraction(interaction, `⌛ Scanning ${files.length} file(s). Please wait.`, true);
+    }
     
 }
 
