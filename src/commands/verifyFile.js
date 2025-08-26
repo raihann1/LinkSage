557fa33fb87db853c6b2bceebe1bcc6c3e37a5b3
raihann1 <72947239+raihann1@users.noreply.github.com>
2025-08-26 19:58:17 -0400

    progress w/ filescan (might change)

--- src/commands/verifyFile.js
+++ src/commands/verifyFile.js
@@ -11,8 +11,34 @@ export async function verifyFile(interaction) {
         return await replyInteraction(interaction, "No files found in the message.", true);
     } else {
          await replyInteraction(interaction, `⌛ Scanning ${files.length} file(s). Please wait.`, true);
+         // use filescan.io api to scan each file
+         const results = [];
+         for(let i = 0; i < files.length; i++) {
+            const fileUrl = files[i];
+            console.log(fileUrl)
+            try {
+                const apiUrl = `https://filescan.io/api/scan/url`;
+                const resp = await fetch(apiUrl, {
+                    method: "POST", 
+                    headers: {
+                        "Content-Type": "application/json",
+                        "X-Api-Key": FS_API_KEY
+                    },
+                    body: JSON.stringify({ 
+                        url: fileUrl, 
+                    })
+                });
+                if (!resp.ok) {
+                    console.log(await resp.json());
+                    throw new Error(`FileScan API error: ${resp.status}`);
+                }
+                const data = await resp.json();
+                console.log(data);
+            } catch (err) {
+                console.error(`FileScan error: ${err.message}`);
+            }
+        }
     }
-    
 }
 
 export const commandInfo = {
